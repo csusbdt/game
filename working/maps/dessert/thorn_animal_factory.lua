d7d3e8bf917ef27c65baf9d7aa71987181566d02
David Turner <susbdt@gmail.com>
2015-12-16 17:28:03 -0800

    X

--- working/maps/dessert/thorn_animal_factory.lua
+++ working/maps/dessert/thorn_animal_factory.lua
@@ -1,61 +1,6 @@
-local textures  = require('res.textures')
-local anim_mt   = require('util.anim_mt')
-
-local thorn_animal_mt = {}
-thorn_animal_mt.__index = thorn_animal_mt
-setmetatable(thorn_animal_mt, anim_mt)
-
-local s = textures.image('maps/dessert/thorn_animal.png')
-
-thorn_animal_mt.anims = {}
-
-thorn_animal_mt.anims['idledown'] = {
-	{ s = s, x =   0, y =   0, w = 64, h = 64, t = 8 }
-}
-
-thorn_animal_mt.anims['idleup'] = {
-	{ s = s, x =   0, y =   0, w = 64, h = 64, t = 8 }
-}
-
-thorn_animal_mt.anims['idleleft'] = {
-	{ s = s, x =   0, y =   0, w = 64, h = 64, t = 8 }
-}
-
-thorn_animal_mt.anims['idleright'] = {
-	{ s = s, x =   0, y =   0, w = 64, h = 64, t = 8 }
-}
-
-thorn_animal_mt.anims['walkdown'] = {
-	{ s = s, x =   0, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x =  64, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 128, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 192, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 128, y =   0, w = 64, h = 64, t = 8 }
-}
-
-thorn_animal_mt.anims['walkup'] = {
-	{ s = s, x =   0, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x =  64, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 128, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 192, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 128, y =   0, w = 64, h = 64, t = 8 }
-}
-
-thorn_animal_mt.anims['walkleft'] = {
-	{ s = s, x =   0, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x =  64, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 128, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 192, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 128, y =   0, w = 64, h = 64, t = 8 }
-}
-
-thorn_animal_mt.anims['walkright'] = {
-	{ s = s, x =   0, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x =  64, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 128, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 192, y =   0, w = 64, h = 64, t = 8 },
-	{ s = s, x = 128, y =   0, w = 64, h = 64, t = 8 }
-}
+local textures        = require('res.textures')
+local anim_mt         = require('util.anim_mt')
+local thorn_animal_mt = require('maps/dessert/thorn_animal_mt')
 
 local function create(o)
 	setmetatable(o, thorn_animal_mt)
